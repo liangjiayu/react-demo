c330fd79818fe794fbcfb2fa70da14052b4f58c7
LiangJiaYu <1013834609@qq.com>
2020-07-15 00:14:16 +0800

    featrue: home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,16 +5,56 @@ import { history } from 'umi';
 // page name =>  home-page
 
 export default () => {
+  function ListItems() {
+    let result = [];
+    for (let i = 0; i < 10; i++) {
+      result.push(
+        <div
+          className="item"
+          key={i}
+          onClick={() => {
+            history.push('/list');
+          }}
+        >
+          <div className="item-img">
+            <img src="https://iph.href.lu/500x500" alt="" />
+          </div>
+          <div className="item-info">
+            <div className="item-title">麦当劳麦当劳</div>
+            <div className="item-subtitle">麦当劳麦当劳</div>
+            <div className="item-more">
+              <div className="cell">
+                <div className="cell-img">
+                  <img src="https://iph.href.lu/500x500" alt="" />
+                </div>
+                <div className="cell-title">麦当劳123</div>
+                <div className="price">￥12</div>
+              </div>
+              <div className="cell">
+                <div className="cell-img">
+                  <img src="https://iph.href.lu/500x500" alt="" />
+                </div>
+                <div className="cell-title">麦当劳123</div>
+                <div className="price">￥12</div>
+              </div>
+              <div className="cell">
+                <div className="cell-img">
+                  <img src="https://iph.href.lu/500x500" alt="" />
+                </div>
+                <div className="cell-title">麦当劳123</div>
+                <div className="price">￥12</div>
+              </div>
+            </div>
+          </div>
+        </div>,
+      );
+    }
+    return <div className="list">{result}</div>;
+  }
+
   return (
     <div className="home-page">
-      <div
-        className="title"
-        onClick={() => {
-          history.push('/list');
-        }}
-      >
-        home
-      </div>
+      <ListItems></ListItems>
     </div>
   );
 };
